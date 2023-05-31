3a75bea9e50bb8d0575d5a0a0a373cc3c6140474
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-31 13:19:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import indexRouter from './routes/index';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-const app = express();
+const server = express();
 
-const port = process.env.PORT || 5000;
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-app.use('/', indexRouter);
-
-module.exports = app;
+export default server;
